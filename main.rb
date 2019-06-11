7a540401937b3d4f0e553cdcb3e4d0e8882cc718
gizemocak <gizem_ocak@outlook.com>
2019-06-11 16:32:59 +0000

    claenups

--- main.rb
+++ main.rb
@@ -6,16 +6,3 @@ require './main.rb'
 
 game = Game.new
 game.run
-
-# puts game
-
-# current_question = game.current_question
-# puts current_question[:question]
-
-# player_answer = gets.chomp
-
-# if player_answer === current_question[:answer].to_s
-#   puts "right"
-# else 
-#   puts "wrong"
-# end
